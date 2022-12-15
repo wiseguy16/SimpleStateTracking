f3f4b2e95d7e5b10377d283d9a3c0e7f33b8eee2
wiseguy16 <gergusa04@gmail.com>
2022-12-15 14:04:25 -0500

    adding my project

---
+++ SimpleStateTracking/SimpleStateTracking/SimpleStateTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStateTrackingApp.swift
+//  SimpleStateTracking
+//
+//  Created by Gregory Weiss on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStateTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
